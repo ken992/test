4a6746aad5f2f19fba7ad13f7f1727b37d8ac448
ken992 <fire09682@gmail.com>
2017-09-17 06:36:57 +0900

    Update template.rb

--- template.rb
+++ template.rb
@@ -1,9 +1,18 @@
+dir = Dir::pwd
+
+########################################
+# Gemfile
+########################################
+gem 'better_errors'
+gem 'binding_of_caller'
+#gem 'byebug'
+
 application <<-APPEND
   config.active_record.default_timezone = :local
   config.time_zone = 'Tokyo'
   config.i18n.default_locale = :ja
 APPEND
 
-gem 'better_errors'
-gem 'binding_of_caller'
-#gem 'byebug'
+inside('config/locales') do
+  run "curl https://raw.githubusercontent.com/svenfuchs/rails-i18n/master/rails/locale/ja.yml > ja.yml"
+end
